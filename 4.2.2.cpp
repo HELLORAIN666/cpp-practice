c6c1ac7b40c54416c16ba2bea94ee6924f02c219
HELLORAIN666 <325979709+HELLORAIN666@users.noreply.github.com>
2026-09-17 19:42:09 +0800

    整理：t1 移入 work 目录，加入 clang-format 配置

---
+++ 4.2.2.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+
+class Person
+{
+public:
+Person()
+{
+    cout<<"Person默认构造函数调用"<<endl;
+}
+Person(const Person& p)
+{
+    int p 
+}
+
+  
+
+};
+
+
+
+
+
+
+int main()
+{
+
+    return 0;
+}
